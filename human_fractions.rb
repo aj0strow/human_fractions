f59888a64810b2132be83d642f2f82d8266aa8d9
AJ Ostrow <ajsonics8@yahoo.com>
2012-09-17 01:08:59 -0400

    refactored code and tests

--- human_fractions.rb
+++ human_fractions.rb
@@ -3,17 +3,18 @@ ALLOWED_BASES = [2, 3, 4, 6, 8]
 class Rational
   
   def round_to_nearest(precision)
-    multiplier = precision.to_r.abs
-    if multiplier == 0
+    if precision == 0
       raise ArgumentError.new 'Rounding to a precision of 0 is not rounding at all!'
     else
+      multiplier = precision.to_r.abs
       (self / multiplier).round * multiplier
-    end    
+    end
   end
   
-  def to_fraction(allowed_bases = ALLOWED_BASES)
-    allowed_bases.map { |b| 
-      self.round_to_nearest Rational(1, b)
+  def to_fraction(*allowed_bases)
+    allowed_bases = ALLOWED_BASES unless allowed_bases.any?
+    allowed_bases.flatten.map { |base| 
+      round_to_nearest Rational(1, base)
     }.sort { |x, y|
       (self - x).abs <=> (self - y).abs
     }.first
@@ -23,12 +24,12 @@ end
 
 class Float
   
-  def to_fraction(allowed_bases = ALLOWED_BASES)
-    self.to_r.to_fraction allowed_bases
+  def to_fraction(*allowed_bases)
+    to_r.to_fraction *allowed_bases
   end
   
   def round_to_nearest(precision)
-    self.to_r.round_to_nearest precision
+    to_r.round_to_nearest precision
   end
   
 end
--- human_fractions_test.rb
+++ human_fractions_test.rb
@@ -3,20 +3,27 @@ require 'test/unit'
 
 class TestFraction < Test::Unit::TestCase
   
-  def test_round_to_nearest
+  def test_rational_round_to_nearest
     assert_equal Rational(1, 4), Rational(26, 100).round_to_nearest('1/4')
     assert_equal Rational(-3, 2), Rational(-76, 48).round_to_nearest('1/2')
+    assert_raise ArgumentError do Rational(1, 2).round_to_nearest(0) end
   end
   
-  def test_humanize
-    assert_equal Rational(1, 2), Rational(2, 3).humanize([2])
-    assert_equal Rational(1, 3), Rational(3, 10).humanize
-    assert_equal Rational(-1, 4), Rational(-27, 101).humanize
+  def test_rational_to_fraction
+    assert_equal Rational(1, 2), Rational(2, 3).to_fraction([2])
+    assert_equal Rational(1, 3), Rational(3, 10).to_fraction
+    assert_equal Rational(-1, 4), Rational(-27, 101).to_fraction
   end
   
-  def test_to_fraction
+  def test_float_round_to_nearet
+    assert_equal Rational(1, 4), (0.251).round_to_nearest('1/4')
+    assert_equal Rational(-3, 2), (-1.498).round_to_nearest('1/2')
+  end
+  
+  def test_float_to_fraction
     assert_equal Rational(1, 3), (1.01 / 3.0).to_fraction
     assert_equal Rational(5, 8), (4.99999 / 7.99998587598).to_fraction
+    assert_equal Rational(1, 4), (0.3).to_fraction( 2, 4 )
   end
   
 end
